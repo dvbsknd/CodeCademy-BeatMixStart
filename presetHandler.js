87c21816cc3d3fa43c5b6d72e455f986af8e6d2a
Dave Baskind <dave@picketstudio.com>
2019-06-05 17:38:47 +1000

    Replaced my work with solution for testing

--- presetHandler.js
+++ presetHandler.js
@@ -2,22 +2,36 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = (requestType, i, newPresetArray) => {
+const getPreset = (index) => {
+  return presets[index] || null;
+};
 
-    if (requestType == 'GET' || requestType == 'PUT') {
-        if (!presets[i]) {
-            return [404];
-        } else {
-            if (requestType === 'GET') {
-            return [200, presets[i]]; 
-            } else if (requestType === 'PUT') {
-                presets[i] = newPresetArray;
-            return [200, presets[i]]; 
-            }
-        }
+const createOrUpdatePreset = (index, array) => {
+  if (!presets[index]) {
+    return;
+  }
+  presets[index] = array;
+  return presets[index];
+};
+
+const presetHandler = (method, index, newPresetArray) => {
+  if (method === 'GET') {
+    let preset = getPreset(index);
+    if (preset) {
+      return [200, preset];
     } else {
-        return [400];
+      return [404];
     }
+  } else if (method === 'PUT') {
+    const newPreset = createOrUpdatePreset(index, newPresetArray);
+    if (newPreset) {
+      return [200, newPreset];
+    } else {
+      return [404];
+    }
+  } else {
+    return [400];
+  }
 };
 
 // Leave this line so that your presetHandler function can be used elsewhere:
--- public/js/script.js
+++ public/js/script.js
@@ -1,47 +1,47 @@
+const createEmptyDrumArray = () => new Array(16).fill(false);
+
 // Drum Arrays
-let kicks = new Array(16).fill(false);
-let snares = new Array(16).fill(false);
-let hiHats = new Array(16).fill(false);
-let rideCymbals = new Array(16).fill(false);
+let kicks = createEmptyDrumArray();
+let snares = createEmptyDrumArray();
+let hiHats = createEmptyDrumArray();
+let rideCymbals = createEmptyDrumArray();
 
-// Function to initialise drum arrays as empty according to provided size
-const initArray = (arrays, size) => {
+const getDrumArrayByName = (name) => {
+  switch (name) {
+    case 'kicks':
+      return kicks;
+    case 'snares':
+      return snares;
+    case 'hiHats':
+      return hiHats;
+    case 'rideCymbals':
+      return rideCymbals;
+    default:
+      return;
+  }
+};
 
-    arrays.forEach(array => {
-        // empty the array
-        while (array.length !== 0) {
-            array.pop();
-        }
-        // fill the array with 'false' entries
-        let i;
-        for (i = 0; i < size; i++) {
-            array.push(false);
-        }
-    });
-    
-}
+const toggleDrum = (drumArrayName, index) => {
+  const drums = getDrumArrayByName(drumArrayName);
+  if (!drums || index > 15 || index < 0) {
+    return;
+  }
+  drums[index] = !drums[index];
+};
 
-// Set all values in an existing array to false
-const clear = (array) => {
-    if (array === 'kicks' || array === 'snares' || array === 'hiHats' || array === 'rideCymbals') {
-        eval(array).fill(false);
-    }
-}
+const clear = (drumArrayName) => {
+  const drums = getDrumArrayByName(drumArrayName);
+  if (drums) {
+    drums.fill(false);
+  }
+};
 
-// Toggles individual drum state
-const toggleDrum = (drum, i) => {
-    if (i < 0 || i > 16) return;
-    if (drum && typeof(i) === 'number') {
-        eval(drum)[i] = !eval(drum)[i];
-    }
-}
-
-// Invert array
-const invert = (array) => {
-    if (array === 'kicks' || array === 'snares' || array === 'hiHats' || array === 'rideCymbals') {
-        array = eval(array);
-        array.forEach((item, index) => {
-            toggleDrum(array, index);
-        })
-    }
-}
+const invert = (drumArrayName) => {
+  const drums = getDrumArrayByName(drumArrayName);
+  if (!drums) {
+    return;
+  }
+  for (let i = 0; i < drums.length; i++) {
+    drums[i] = !drums[i];
+  }
+};
