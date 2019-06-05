d0ff4cf6cd642652f47c8b4491b2955c4039fec1
Dave Baskind <dave@picketstudio.com>
2019-06-05 18:10:04 +1000

    Bonus question completed

--- public/js/script.js
+++ public/js/script.js
@@ -45,3 +45,20 @@ const invert = (drumArrayName) => {
     drums[i] = !drums[i];
   }
 };
+
+const getNeighborPads = (x, y, size) => {
+  // Abort if home is not on the range
+  if (x < 0 || x > size - 1 || y < 0 || y > size - 1) { 
+    return [];
+  }
+  // Set neighbour values based on adding/subtracting from current values
+  const top = [x, y + 1];
+  const right = [x + 1, y];
+  const bottom = [x, y - 1];
+  const left = [x -1, y];
+  // Remove any coordinates that are outside the boundaries
+  let neighbours = [top, right, bottom, left].filter(neighbour => {
+    return neighbour[0] <= size - 1 && neighbour[0] >= 0 && neighbour[1] <= size - 1 && neighbour[1] >= 0;
+  });
+  return neighbours;
+}
