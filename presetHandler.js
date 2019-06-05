ed2cfb2d853376588bb07363afe1f5896902b415
Dave Baskind <dave@picketstudio.com>
2019-06-05 17:10:40 +1000

    First commit of the server-side code

--- presetHandler.js
+++ presetHandler.js
@@ -2,7 +2,23 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (requestType, i, newPresetArray) => {
+
+    if (requestType == 'GET' || requestType == 'PUT') {
+        if (!presets[i]) {
+            return [404];
+        } else {
+            if (requestType === 'GET') {
+            return [200, presets[i]]; 
+            } else if (requestType === 'PUT') {
+                presets[i] = newPresetArray;
+            return [200, presets[i]]; 
+            }
+        }
+    } else {
+        return [400];
+    }
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
